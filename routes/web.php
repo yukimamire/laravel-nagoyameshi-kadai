a82ab05e669943f0e9fb954092115246b8836a4c
yuu5m77@gmail.com <yuu5m77@gmail.com>
2024-08-29 03:46:19 +0900

    会員側の認証を実装

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+require __DIR__.'/auth.php';
