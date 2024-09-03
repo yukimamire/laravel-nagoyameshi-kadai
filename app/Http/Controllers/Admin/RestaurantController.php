2dbeba2d09fea010ed65c2ab2bbe19cebd6a32e1
yuu5m77@gmail.com <yuu5m77@gmail.com>
2024-09-03 20:40:23 +0900

    変更

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\category;
 use App\Models\Restaurant;
 use App\Models\Category;
 
