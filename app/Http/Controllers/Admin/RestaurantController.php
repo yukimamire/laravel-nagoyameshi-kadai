e99ba4ae8416fca0bf876db58432111c89dde23c
yuu5m77@gmail.com <yuu5m77@gmail.com>
2024-09-03 20:26:09 +0900

    カテゴリ設定

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Restaurant;
+use App\Models\Category;
 
 class RestaurantController extends Controller
 {
